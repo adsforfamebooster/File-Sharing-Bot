58d5b6bf9eb1918007374c36b5e4a517742695de
adsforfamebooster <153657573+adsforfamebooster@users.noreply.github.com>
2024-08-25 19:57:14 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b><i>○ Creater : <a href='https://t.me/cuties_hub'>Admin Of Channel</a>\n</i></b>",
+            text = f"<b>I'm A Advance File Storing Bot</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
